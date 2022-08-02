7cd405dff77a6678953c4d8d99d4c44bce2861d0
Alex Cent <alex_cent@mailbox.org>
2022-08-02 19:12:31 +0300

    Add fancy selection indicators

--- js/rps.js
+++ js/rps.js
@@ -10,11 +10,18 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function playRound(e) {
+async function playRound(e) {
   let playerChoice = getPlayerChoice(e);
   let computerChoice = getComputerChoice();
+  setChoice(playerChoice, true);
+  setSelection(playerChoice, true);
+  setSelection(computerChoice);
+  setChoice(computerChoice);
   let winner = doesPlayerWin(playerChoice, computerChoice);
   changeScore(winner);
+  await sleep(2000);
+  unsetSelection();
+  unsetChoice();
 }
 
 function getPlayerChoice(e) {
@@ -49,3 +56,61 @@ function changeScore(playerWin) {
   score.innerText = +score.innerText + 1;
 }
 
+function setSelection(cardName, isPlayer = false) {
+  const card = document.getElementById(cardName);
+  card.classList.remove('other');
+  card.classList.add('selected');
+  if (isPlayer) {
+    for (let i = 0; i < CHOICES.length; i++) {
+      setOther(CHOICES[i]);
+      setRelation(CHOICES[i], cardName);
+    }
+  }
+}
+
+function setOther(cardName) {
+  const card = document.getElementById(cardName);
+  if (!card.classList.contains('selected')) {
+    card.classList.add('other');
+  }
+}
+
+function setRelation(cardName, mainCardName) {
+  const card = document.getElementById(cardName);
+  let relation = doesPlayerWin(mainCardName, cardName);
+  if (relation === null) {
+    card.classList.add('tie');
+  } else if (relation) {
+    card.classList.add('win');
+  } else {
+    card.classList.add('lose');
+  }
+}
+
+function unsetSelection() {
+  const SELECTION_CLASSES = ['selected', 'other', 'tie', 'win', 'lose'];
+  CHOICES.forEach((name) => document.getElementById(name).classList.remove(...SELECTION_CLASSES));
+}
+
+function setChoice(cardName, isPlayer = false) {
+  const card = document.getElementById(cardName);
+  const wrapper = document.createElement('div');
+  const name = document.createElement('div');
+  if (isPlayer) {
+    wrapper.classList.add('player');
+    name.innerText = 'You';
+  } else {
+    wrapper.classList.add('opponent');
+    name.innerText = 'Computer';
+  }
+  name.classList.add('name');
+  wrapper.appendChild(name)
+  card.appendChild(wrapper);
+}
+
+function unsetChoice() {
+  const player = document.querySelector('.card .player');
+  if (player) player.remove();
+  const opponent = document.querySelector('.card .opponent');
+  if (opponent) opponent.remove();
+}
